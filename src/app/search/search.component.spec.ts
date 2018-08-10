b8ebf50febc6faac2a244daa718e3f4452d58a65
jdu <jdu.xian@gmail.com>
2018-08-10 10:21:54 +0800

    add test for validate number

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -23,4 +23,45 @@ describe('SearchComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('should validate number', () => {
+    it('failed when search input length is not 10', () => {
+      const hostElement = fixture.nativeElement;
+      const searchInput: HTMLInputElement = hostElement.querySelector('input');
+
+      searchInput.value = '111';
+      searchInput.dispatchEvent(new Event('input'));
+      searchInput.dispatchEvent(new Event('keyup'));
+      fixture.detectChanges();
+
+      const errorMessage: HTMLElement = hostElement.querySelector('.error-message');
+      expect(errorMessage.textContent).toBe('请输入10位有效数字单号');
+    });
+
+    it('failed when search input is not number', () => {
+      const hostElement = fixture.nativeElement;
+      const searchInput: HTMLInputElement = hostElement.querySelector('input');
+
+      searchInput.value = '123456789a';
+      searchInput.dispatchEvent(new Event('input'));
+      searchInput.dispatchEvent(new Event('keyup'));
+      fixture.detectChanges();
+
+      const errorMessage: HTMLElement = hostElement.querySelector('.error-message');
+      expect(errorMessage.textContent).toBe('请输入10位有效数字单号');
+    });
+
+    it('success when search input is 10 length of number', () => {
+      const hostElement = fixture.nativeElement;
+      const searchInput: HTMLInputElement = hostElement.querySelector('input');
+
+      searchInput.value = '1234567890';
+      searchInput.dispatchEvent(new Event('input'));
+      searchInput.dispatchEvent(new Event('keyup'));
+      fixture.detectChanges();
+
+      const errorMessage: HTMLElement = hostElement.querySelector('.error-message');
+      expect(errorMessage).toBeNull();
+    });
+  });
 });
